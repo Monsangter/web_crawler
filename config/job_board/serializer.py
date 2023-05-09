7bf82356d48dbe2d9ec556c6d64cddbf6420197d
Monsangter <97347454+Monsangter@users.noreply.github.com>
2023-05-10 01:09:20 +0900

    Update serializer.py

--- config/job_board/serializer.py
+++ config/job_board/serializer.py
@@ -1,1 +1,19 @@
+from rest_framework import serializers
+from .models import Wanted, Jobkorea, Saramin
+
+class WantedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wanted
+        fields = '__all__'
+
+class JobkoreaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Jobkorea
+        fields = '__all__'
+
+class SaraminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Saramin
+        fields = '__all__'
+
 
