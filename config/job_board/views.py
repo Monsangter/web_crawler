b26ba560e49fcf2f77f5a835447b98e5c5d37270
Monsangter <97347454+Monsangter@users.noreply.github.com>
2023-05-10 01:09:47 +0900

    Update views.py

--- config/job_board/views.py
+++ config/job_board/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Wanted, Jobkorea, Saramin
+from .serializers import WantedSerializer, JobkoreaSerializer, SaraminSerializer
 
-# Create your views here.
+class WantedViewSet(viewsets.ModelViewSet):
+    queryset = Wanted.objects.all()
+    serializer_class = WantedSerializer
+
+class JobkoreaViewSet(viewsets.ModelViewSet):
+    queryset = Jobkorea.objects.all()
+    serializer_class = JobkoreaSerializer
+
+class SaraminViewSet(viewsets.ModelViewSet):
+    queryset = Saramin.objects.all()
+    serializer_class = SaraminSerializer
+    
