7b723479c51571a33f2f47d2665bc9365c753d40
Monsangter <97347454+Monsangter@users.noreply.github.com>
2023-05-09 16:08:08 +0900

    Update models.py

--- config/job_board/models.py
+++ config/job_board/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Wanted(models.Model):
+    company = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    url = models.TextField()
+    location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('company', 'position')
+
 # Create your models here.
+
