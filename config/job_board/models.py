f73a3e9855270cab7f8c53b41880c3261cf91971
Monsangter <97347454+Monsangter@users.noreply.github.com>
2023-05-09 16:45:30 +0900

    Update models.py

--- config/job_board/models.py
+++ config/job_board/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+# Create your models here.
+
 class Wanted(models.Model):
     company = models.CharField(max_length=100)
     position = models.CharField(max_length=100)
@@ -10,5 +12,25 @@ class Wanted(models.Model):
     class Meta:
         unique_together = ('company', 'position')
 
-# Create your models here.
+class Jobkorea(models.Model):
+    company = models.CharField(max_length=100)
+    detail = models.CharField(max_length=100)
+    url = models.TextField()
+    exp = models.CharField(max_length=20)
+    location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ('company', 'position')
+        
+class Saramin(models.Model):
+    company = models.CharField(max_length=100)
+    detail = models.CharField(max_length=100)
+    url = models.TextField()
+    exp = models.CharField(max_length=20)
+    location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ('company', 'position')
 
